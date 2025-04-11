ce92388ea352a2d923078c0417229f9892fe1e14
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2025-04-11 18:08:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
-intents.message_content = True  # Required for reading messages
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="/", intents=intents)
 
